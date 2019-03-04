23ca69fa44f9314c29ad7205a2ec6ce451d80df9
Hamza Zerouali <hzeroual@everis.com>
2019-03-04 17:31:02 +0100

    04/03/2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component'
 import { HeroAddComponent } from './hero-add/hero-add.component';
 import { HeroUpdateComponent } from './hero-update/hero-update.component';
 const routes: Routes = [
-  //{ path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent },
   { path: 'heroes',     component: HeroesComponent,
     children:[
